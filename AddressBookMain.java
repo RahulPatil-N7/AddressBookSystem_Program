c56980c7b3e87967a3655182dca02beb257d5797
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-06-28 01:39:27 +0530

    Updated : Add contact details in Address book.

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,7 +4,9 @@ public class AddressBookMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book System program.");
-
+		 AddressBook addressbook = new  AddressBook();
+		 addressbook.addContact();
+		
 	}
 
 }
